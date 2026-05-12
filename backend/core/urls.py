4214fa9c9ce274bfa3342ee79cdca07439958aee
vanhieu251206 <vanhieu25122006@gmail.com>
2026-05-13 01:52:23 +0700

    Xây dựng API đăng ký và đăng nhập JWT

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.views import health_check
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/health/', health_check),
+
+    path('api/auth/', include('accounts.urls')),
 ]
